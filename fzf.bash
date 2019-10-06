603b05ce03170171655907c19b8025f277ab2608
phantasea <phantasea@gmail.com>
2019-10-06 18:00:43 +0300

    fzf.bash changed

--- fzf.bash
+++ fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/apps/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/apps/fzf/bin"
+if [[ ! "$PATH" == */opt/misc/apps/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/misc/apps/fzf/bin"
 fi
 
 export FZF_DEFAULT_OPTS="--exact --multi --cycle --bind=ctrl-f:page-down,ctrl-b:page-up --color=fg+:7,bg+:1,hl+:3"
@@ -9,8 +9,8 @@ export FZF_COMPLETION_TRIGGER=',,'
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/apps/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/misc/apps/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/apps/fzf/shell/key-bindings.bash"
+source "/opt/misc/apps/fzf/shell/key-bindings.bash"
