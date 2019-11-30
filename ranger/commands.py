0e6bb0eead99ac4cc167d642173b9ffac9f24b12
phantasea <phantasea@gmail.com>
2019-11-30 18:03:39 +0200

    ranger/commands.py changed

--- ranger/commands.py
+++ ranger/commands.py
@@ -40,23 +40,57 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
 # fzf_locate
 class fzf_locate(Command):
-    """
-    :fzf_locate
-
-    Find a file using fzf.
-
-    With a prefix argument select only directories.
-
-    See: https://github.com/junegunn/fzf
-    """
     def execute(self):
         import subprocess
         if self.quantifier:
             command="cat ~/.favedirs | sed \"s%\~%$HOME%\" | xargs locate | fzf | xargs -r vimux"
         else:
             command="cat ~/.favedirs | sed \"s%\~%$HOME%\" | xargs locate | fzf | xargs -r vimux"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+# fzf_goto
+class fzf_goto(Command):
+    def execute(self):
+        import subprocess
+        command="fd -a -t=f . | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+# fzf_edit
+class fzf_edit(Command):
+    def execute(self):
+        import subprocess
+        command="fd -a -t=f . | fzf | xargs -r vimux"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+# fzf_fave
+class fzf_fave(Command):
+    def execute(self):
+        import subprocess
+        command="cat ~/.favedirs | sed \"s%\~%$HOME%\" | xargs fd -a -t=f . | fzf | xargs -r vimux"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
