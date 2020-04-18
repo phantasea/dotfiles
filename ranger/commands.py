b052257282568a7212f4b98ce7994c11418e82ca
phantasea <phantasea@gmail.com>
2020-04-18 15:40:56 +0300

    ranger/commands.py: add more commands

--- ranger/commands.py
+++ ranger/commands.py
@@ -4,12 +4,59 @@
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import *
-
-# A simple command for demonstration purposes follows.
-#------------------------------------------------------------------------------
+from collections import deque
 
 # You can import any python module as needed.
 import os
+
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+
+    Creates a directory with the name <dirname> and enters it.
+    """
+
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search('^/|^~[^/]*/', dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
+                else:
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+class toggle_flat(Command):
+    """
+    :toggle_flat
+
+    Flattens or unflattens the directory view.
+    """
+
+    def execute(self):
+        if self.fm.thisdir.flat == 0:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = -1
+            self.fm.thisdir.load_content()
+        else:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = 0
+            self.fm.thisdir.load_content()
 
 class fzf_select(Command):
     """
@@ -146,3 +193,100 @@ class fzf_porn(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+fd_deq = deque()
+class fd_find(Command):
+    """:fd_find [-d<depth>] <query>
+
+    Executes "fd -d<depth> <query>" in the current directory and focuses the
+    first match. <depth> defaults to 2, i.e. only the contents of the current
+    directory.
+    """
+
+    def execute(self):
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+        if not 'fd' in get_executables():
+            self.fm.notify("Couldn't find fd on the PATH.", bad=True)
+            return
+        if self.arg(1):
+            if self.arg(1)[:2] == '-d':
+                depth = self.arg(1)
+                target = self.rest(2)
+            else:
+                depth = '-d1'
+                target = self.rest(1)
+        else:
+            self.fm.notify(":fd_find [-d<depth>] <query>", bad=True)
+            return
+
+        # For convenience, change which dict is used as result_sep to change
+        # fd's behavior from splitting results by \0, which allows for newlines
+        # in your filenames to splitting results by \n, which allows for \0 in
+        # filenames.
+        null_sep = {'arg': '-0', 'split': '\0'}
+        nl_sep = {'arg': '', 'split': '\n'}
+        result_sep = null_sep
+
+        process = subprocess.Popen(['fd', result_sep['arg'], depth, target],
+                    universal_newlines=True, stdout=subprocess.PIPE)
+        (search_results, _err) = process.communicate()
+        global fd_deq
+        fd_deq = deque((self.fm.thisdir.path + os.sep + rel for rel in
+            sorted(search_results.split(result_sep['split']), key=str.lower)
+            if rel != ''))
+        if len(fd_deq) > 0:
+            self.fm.select_file(fd_deq[0])
+
+class fd_next(Command):
+    """:fd_next
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(-1) # rotate left
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+class fd_prev(Command):
+    """:fd_prev
+
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_deq) > 1:
+            fd_deq.rotate(1) # rotate right
+            self.fm.select_file(fd_deq[0])
+        elif len(fd_deq) == 1:
+            self.fm.select_file(fd_deq[0])
+
+class fzf_rga(Command):
+    """
+    :fzf_rga
+    Search in PDFs, E-Books and Office documents in current directory.
+    Allowed extensions: .epub, .odt, .docx, .fb2, .ipynb, .pdf.
+
+    Usage: fzf_rga <search string>
+    """
+    def execute(self):
+        if self.arg(1):
+            search_string = self.rest(1)
+        else:
+            self.fm.notify("Usage: fzf_rga <search string>", bad=True)
+            return
+
+        import subprocess
+        import os.path
+        from ranger.container.file import File
+        #command="rga '%s' . --rga-adapters=pandoc,poppler | fzf +m | awk -F':' '{print $1}'" % search_string
+        command="rg '%s' . | fzf +m | awk -F':' '{print $1}'" % search_string
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.execute_file(File(fzf_file))
+
