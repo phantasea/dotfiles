9270a9c8faff57fe7b24b3cb5b2388e28e17591d
phantasea <phantasea@gmail.com>
2020-04-19 18:14:59 +0300

    ranger/scope.sh changed

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -364,7 +364,7 @@ handle_mime() {
             fi
             env HIGHLIGHT_OPTIONS="${HIGHLIGHT_OPTIONS}" highlight \
                 --out-format="${highlight_format}" \
-                --force -- "${FILE_PATH}" && exit 5
+                --force --quiet -- "${FILE_PATH}" && exit 5
             env COLORTERM=8bit bat --color=always --style="plain" \
                 -- "${FILE_PATH}" && exit 5
             pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
