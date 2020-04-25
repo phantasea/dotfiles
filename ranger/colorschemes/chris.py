2eafa9214f7bc606f53655aed3fc4ba440ddc057
phantasea <phantasea@gmail.com>
2020-04-25 14:32:50 +0300

    ranger/colorschemes/ changed including retab

--- ranger/colorschemes/chris.py
+++ ranger/colorschemes/chris.py
@@ -17,116 +17,158 @@ from ranger.gui.colorscheme import ColorScheme
 from ranger.gui.color import *
 
 class Default(ColorScheme):
-	def use(self, context):
-		fg, bg, attr = default_colors
-		fg = yellow
+    def use(self, context):
+        fg, bg, attr = default_colors
+        fg = yellow
 
-		if context.reset:
-			return default_colors
+        if context.reset:
+            return default_colors
 
-		elif context.in_browser:
-			if context.selected:
-				attr = reverse
-			else:
-				attr = normal
-			if context.empty or context.error:
-				bg = red
-				fg = black
-			if context.border:
-				fg = default
-			if context.media:
-				if context.image:
-					fg = yellow
-				else:
-					fg = yellow
-			if context.container:
-				fg = red
-			if context.directory:
-				#attr |= bold
-				fg = cyan
-			elif context.executable and not \
-					any((context.media, context.container,
-						context.fifo, context.socket)):
-				#attr |= bold
-				fg = green
-			if context.socket:
-				fg = magenta
-				attr |= bold
-			if context.fifo or context.device:
-				fg = yellow
-				if context.device:
-					attr |= bold
-			if context.link:
-				#fg = context.good and magenta
-				if context.good:
-					attr |= bold
-				fg = magenta
-			if context.tag_marker and not context.selected:
-				attr |= bold
-				if fg in (red, magenta):
-					fg = white
-				else:
-					fg = red
-			if not context.selected and (context.cut or context.copied):
-				fg = black
-				attr |= bold
-			if context.main_column:
-				if context.selected:
-					attr |= bold
-				if context.marked:
-					attr |= bold
-					fg = white
-			if context.badinfo:
-				if attr & reverse:
-					bg = magenta
-				else:
-					fg = magenta
+        elif context.in_browser:
+            if context.selected:
+                attr = reverse
+            else:
+                attr = normal
+            if context.empty or context.error:
+                bg = red
+                fg = black
+            if context.border:
+                fg = default
+            if context.media:
+                if context.image:
+                    fg = yellow
+                else:
+                    fg = yellow
+            if context.container:
+                fg = red
+            if context.directory:
+                #attr |= bold
+                fg = cyan
+            elif context.executable and not \
+                    any((context.media, context.container,
+                        context.fifo, context.socket)):
+                #attr |= bold
+                fg = green
+            if context.socket:
+                fg = magenta
+                attr |= bold
+            if context.fifo or context.device:
+                fg = yellow
+                if context.device:
+                    attr |= bold
+            if context.link:
+                #fg = context.good and magenta
+                if context.good:
+                    attr |= bold
+                fg = magenta
+            if context.tag_marker and not context.selected:
+                attr |= bold
+                if fg in (red, magenta):
+                    fg = white
+                else:
+                    fg = red
+            if not context.selected and (context.cut or context.copied):
+                fg = black
+                attr |= bold
+            if context.main_column:
+                if context.selected:
+                    attr |= bold
+                if context.marked:
+                    attr |= bold
+                    fg = white
+            if context.badinfo:
+                if attr & reverse:
+                    bg = magenta
+                else:
+                    fg = magenta
 
-		elif context.in_titlebar:
-			attr |= bold
-			if context.hostname:
-				fg = context.bad and red or green
-				bg = red
-			elif context.directory:
-				fg = white
-				bg = red
-			elif context.file:
-				fg = white
-				bg = red
-			elif context.tab:
-				if context.good:
-					bg = red
-			elif context.link:
-				fg = white
-				bg = red
-			fg = white
+        elif context.in_titlebar:
+            attr |= bold
+            if context.hostname:
+                fg = context.bad and red or green
+                bg = red
+            elif context.directory:
+                fg = white
+                bg = red
+            elif context.file:
+                fg = white
+                bg = red
+            elif context.tab:
+                if context.good:
+                    bg = red
+            elif context.link:
+                fg = white
+                bg = red
+            fg = white
 
-		elif context.in_statusbar:
-			if context.permissions:
-				if context.good:
-					fg = cyan
-				elif context.bad:
-					fg = magenta
-			if context.marked:
-				attr |= bold | reverse
-				fg = yellow
-			if context.message:
-				if context.bad:
-					attr |= bold
-					fg = red
-			bg = red
-			fg = white
-			attr |= bold
+        elif context.in_statusbar:
+            bg = red
+            fg = white
+            attr |= bold
+            if context.marked:
+                attr |= bold | reverse
+                fg += BRIGHT
+            if context.frozen:
+                attr |= bold | reverse
+                fg = cyan
+                fg += BRIGHT
+            if context.message:
+                if context.bad:
+                    attr |= bold
+                    fg = red
+                    fg += BRIGHT
+            if context.vcsinfo:
+                fg = black
+                bg = green
+                attr &= ~bold
+            if context.vcscommit:
+                fg = black
+                bg = green
+                attr &= ~bold
+            if context.vcsdate:
+                fg = black
+                bg = green
+                attr &= ~bold
 
-		if context.text:
-			if context.highlight:
-				attr |= reverse
+        if context.text:
+            if context.highlight:
+                attr |= reverse
 
-		if context.in_taskview:
-			if context.title:
-				fg = cyan
+        if context.in_taskview:
+            if context.title:
+                fg = cyan
 
-			if context.selected:
-				attr |= reverse
+            if context.selected:
+                attr |= reverse
 
-		return fg, bg, attr
+        if context.vcsfile and not context.selected:
+            attr &= ~bold
+            if context.vcsconflict:
+                fg = magenta
+            elif context.vcsuntracked:
+                fg = cyan
+            elif context.vcschanged:
+                fg = red
+            elif context.vcsunknown:
+                fg = red
+            elif context.vcsstaged:
+                fg = green
+            elif context.vcssync:
+                fg = green
+            elif context.vcsignored:
+                fg = default
+
+        elif context.vcsremote and not context.selected:
+            attr &= ~bold
+            if context.vcssync or context.vcsnone:
+                fg = green
+            elif context.vcsbehind:
+                fg = red
+            elif context.vcsahead:
+                fg = blue
+            elif context.vcsdiverged:
+                fg = magenta
+            elif context.vcsunknown:
+                fg = red
+
+        return fg, bg, attr
