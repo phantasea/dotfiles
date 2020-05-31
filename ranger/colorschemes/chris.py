9796570920ff951ac3a1745771fed09473ff5e5b
phantasea <phantasea@gmail.com>
2020-05-31 14:23:46 +0300

    ranger: add context.text and context.special highlight

--- ranger/colorschemes/chris.py
+++ ranger/colorschemes/chris.py
@@ -1,22 +1,17 @@
-# Copyright (C) 2009, 2010, 2011  Roman Zimbelmann <romanz@lavabit.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This file is part of ranger, the console file manager.
+# License: GNU GPL version 3, see the file "AUTHORS" for details.
+
+from __future__ import (absolute_import, division, print_function)
 
 from ranger.gui.colorscheme import ColorScheme
-from ranger.gui.color import *
+from ranger.gui.color import (
+    black, blue, cyan, green, magenta, red, white, yellow, default,
+    normal, bold, reverse, dim, BRIGHT,
+    default_colors,
+)
 
-class Default(ColorScheme):
+
+class Chris(ColorScheme):
     def use(self, context):
         fg, bg, attr = default_colors
         fg = yellow
@@ -37,10 +32,21 @@ class Default(ColorScheme):
             if context.media:
                 if context.image:
                     fg = yellow
+                elif context.audio:
+                    fg = yellow
+                elif context.video:
+                    fg = yellow
                 else:
                     fg = yellow
             if context.container:
                 fg = red
+            if context.text:
+                fg = white
+            if context.document:
+                fg = red
+            if context.special:
+                fg = red
+                attr |= bold
             if context.directory:
                 #attr |= bold
                 fg = cyan
