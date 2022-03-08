a633956250db5a1d7ad1d2ae7d1e99cbeeff5fbf
phantasea <phantasea@gmail.com>
2022-03-08 20:46:48 +0200

    ranger/vifm config minor change

--- ranger/commands.py
+++ ranger/commands.py
@@ -209,7 +209,7 @@ class fzf_fave(Command):
 class fzf_vids(Command):
     def execute(self):
         import subprocess
-        command="fd --hidden -d=2 -t=f -e=mp4 -e=webm -e=avi -e=mkv -e=rmvb . ~/vids | \
+        command="fd -H -L -d=2 -t=f -e=mp4 -e=webm -e=avi -e=mkv -e=rmvb . ~/vids | \
                 fzf --height=0 --bind 'ctrl-o:execute(fzfopen {}),j:down,k:up' | \
                 sed 's/ /\\ /g' | xargs -r fileopen"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
