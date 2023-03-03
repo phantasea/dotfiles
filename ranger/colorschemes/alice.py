e844dcd01e376b1baa08503cc0c77dbe5db3f946
phantasea <phantasea@gmail.com>
2023-03-04 00:51:42 +0200

    moc/ranger/vifm/tridacty changed

--- ranger/colorschemes/alice.py
+++ ranger/colorschemes/alice.py
@@ -1,111 +1,208 @@
-# Joseph Tannhuber <sepp.tannhuber@yahoo.de>, 2013
-# Solarized like colorscheme, similar to solarized-dircolors
-# from https://github.com/seebi/dircolors-solarized.
-# This is a modification of Roman Zimbelmann's default colorscheme.
-# This software is distributed under the terms of the GNU GPL version 3.
+# This file is part of ranger, the console file manager.
+# License: GNU GPL version 3, see the file "AUTHORS" for details.
+
+from __future__ import (absolute_import, division, print_function)
 
 from ranger.gui.colorscheme import ColorScheme
-from ranger.gui.color import *
+from ranger.gui.color import (
+    black, blue, cyan, green, magenta, red, white, yellow, default,
+    normal, bold, reverse, dim, BRIGHT,
+    default_colors,
+)
 
-class Solarized(ColorScheme):
-    progress_bar_color = blue
 
+class Chris(ColorScheme):
     def use(self, context):
         fg, bg, attr = default_colors
+        fg = 185
 
         if context.reset:
             return default_colors
 
         elif context.in_browser:
-            fg = 244
             if context.selected:
                 attr = reverse
             else:
                 attr = normal
-            if context.empty or context.error:
-                bg = red
+            if context.error:
+                bg = 198
+                fg = black
+            if context.empty:
+                attr |= bold
+                fg = black
             if context.border:
                 fg = default
             if context.media:
                 if context.image:
-                    fg = 136
+                    fg = 185
+                elif context.audio:
+                    fg = 185
+                elif context.video:
+                    fg = 185
                 else:
-                    fg = 166
+                    fg = 185
             if context.container:
-                fg = 61
+                fg = 198
+            if context.mimetext:
+                fg = 185
+            if context.document:
+                fg = 185
+            if context.special:
+                fg = 198
+                attr |= bold
             if context.directory:
-                fg = 33
+                #attr |= bold
+                fg = 39
             elif context.executable and not \
                     any((context.media, context.container,
                         context.fifo, context.socket)):
-                fg = 64
-                attr |= bold
+                #attr |= bold
+                fg = 115
             if context.socket:
-                fg = 136
-                bg = 230
+                fg = 207
                 attr |= bold
-            if context.fifo:
-                fg = 136
-                bg = 230
-                attr |= bold
-            if context.device:
-                fg = 244
-                bg = 230
-                attr |= bold
+            if context.fifo or context.device:
+                fg = 185
+                if context.device:
+                    attr |= bold
             if context.link:
-                fg = context.good and 37 or 160
-                bg = context.bad and 235
                 attr |= bold
+                if context.good:
+                    fg = 207
+                else:
+                    fg = black
             if context.tag_marker and not context.selected:
                 attr |= bold
-                if fg in (red, magenta):
+                if fg in (198, 207):
                     fg = white
                 else:
-                    fg = red
-            if not context.selected and (context.cut or context.copied):
-                fg = 234
-                attr |= bold
+                    fg = 198
+            if context.line_number:
+                fg = white
+                if not context.selected:
+                    attr &= ~bold
+                else:
+                    fg = white
+                    attr |= bold | reverse
+            if context.line_number_separator:
+                fg, bg, attr = default_colors
             if context.main_column:
                 if context.selected:
                     attr |= bold
                 if context.marked:
                     attr |= bold
-                    bg = 237
+                    fg = white
             if context.badinfo:
                 if attr & reverse:
-                    bg = magenta
+                    bg = 207
                 else:
-                    fg = magenta
+                    fg = 207
+            #if not context.selected and (context.cut or context.copied):
+            if context.cut or context.copied:
+                fg = black
+                attr |= bold
 
         elif context.in_titlebar:
-            attr |= bold
+            #attr |= bold
             if context.hostname:
-                fg = context.bad and 160 or 93
-                bg = context.bad and 235
-            elif context.directory:
-                fg = 33
-            elif context.tab:
-                fg = context.good and 47 or 33
-                bg = 239
+                attr |= bold
+                fg = context.bad and 198 or 115
+                bg = 198
+                fg = white
             elif context.link:
-                fg = cyan
+                fg = black
+                bg = 207
+            elif context.directory:
+                fg = black
+                bg = 39
+            elif context.file:
+                fg = black
+                bg = 185
+            elif context.keybuffer:
+                bg = 115
+                fg = black
+            elif context.tab:
+                attr |= bold
+                fg = white
+                if context.good:
+                    bg = 198
 
         elif context.in_statusbar:
+            #attr |= bold
             if context.permissions:
-                if context.good:
-                    fg = 93
-                elif context.bad:
-                    fg = 160
-            if context.marked:
+                bg = 198
+                fg = black
+            elif context.owner or context.group:
+                bg = 207
+                fg = black
+            elif context.mtime:
+                bg = 115
+                fg = black
+            elif context.link:
+                bg = 63
+                fg =white
+                attr |= bold
+            elif context.lspace:
+                bg = white
+                fg = black
+            elif context.mspace:
+                bg = 63
+                fg = black
+            elif context.rspace:
+                bg = white
+                fg = black
+            elif context.flat:
+                bg = 115
+                fg = black
+            elif context.filter:
+                bg = 185
+                fg = black
+            elif context.size:
+                bg = 185
+                fg = black
+            elif context.stars:
+                bg = 207
+                fg = black
+            elif context.ruler or context.percentage or context.top or context.bot or context.all:
+                bg = 185
+                fg = black
+            elif context.marked:
+                bg = 198
+                fg = black
+            elif context.systime:
+                bg = 115
+                fg = black
+            elif context.frozen:
                 attr |= bold | reverse
-                fg = 237
-            if context.message:
+                fg = 39
+                fg += BRIGHT
+            elif context.loaded:
+                bg = white
+                fg = black
+                attr |= bold
+            elif context.message:
+                fg = 115
                 if context.bad:
-                    attr |= bold
-                    fg = 160
-                    bg = 235
-            if context.loaded:
-                bg = self.progress_bar_color
+                    fg = 198
+                attr |= reverse
+                fg += BRIGHT
+            elif context.vcsinfo:
+                bg = 63
+                fg = black
+                attr &= ~bold
+            elif context.vcscommit:
+                bg = 185
+                fg = black
+                attr &= ~bold
+            elif context.vcsdate:
+                bg = 39
+                fg = black
+                attr &= ~bold
+
+        elif context.in_console:
+            bg = 198
+            fg = white
+            attr |= bold
 
         if context.text:
             if context.highlight:
@@ -113,15 +210,39 @@ class Solarized(ColorScheme):
 
         if context.in_taskview:
             if context.title:
-                fg = 93
+                fg = 39
 
             if context.selected:
                 attr |= reverse
 
-            if context.loaded:
-                if context.selected:
-                    fg = self.progress_bar_color
-                else:
-                    bg = self.progress_bar_color
+        if context.vcsfile and not context.selected:
+            attr &= ~bold
+            if context.vcsconflict:
+                fg = 207
+            elif context.vcsuntracked:
+                fg = 198
+            elif context.vcschanged:
+                fg = 198
+            elif context.vcsunknown:
+                fg = 198
+            elif context.vcsstaged:
+                fg = 115
+            elif context.vcssync:
+                fg = 115
+            elif context.vcsignored:
+                fg = white
+
+        elif context.vcsremote and not context.selected:
+            attr &= ~bold
+            if context.vcssync or context.vcsnone:
+                fg = 115
+            elif context.vcsbehind:
+                fg = 198
+            elif context.vcsahead:
+                fg = 63
+            elif context.vcsdiverged:
+                fg = 207
+            elif context.vcsunknown:
+                fg = 198
 
         return fg, bg, attr
