33c38cd884aba9772bf31d176b1b7e7a4af9c29c
phantasea <phantasea@gmail.com>
2023-05-09 21:11:09 +0300

    fzf cmds improved

--- fzf.bash
+++ fzf.bash
@@ -61,13 +61,15 @@ ffenv() {
 
 ffj() { cd "$(cat ~/.config/z.data | cut -d'|' -f 1 | fzf +m --inline-info)" ; }
 ffs() { surfraw "$(cat ~/.config/surfraw/bookmarks | sed '/^$/d' | sort -n | fzf -e -i)" ; }
-fff() { cat ~/.favedirs | sed "s%\~%$HOME%" | xargs fd -a -t=f -t=l . | fzf "$@" | xargs -r fileopen ;}
 ffl() { cat ~/.favedirs | sed "s%\~%$HOME%" | xargs locate | fzf "$@" | xargs -r fileopen ;}
-ffa() { fd -d=2 -t=f -e=mp3 . ~/auds | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
-ffv() { fd -H -d=2 -t=f -e=mp4 -e=webm -e=avi -e=mkv -e=rmvb . ~/vids | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
-ffw() { fd -t=f -d=1 . ~/docs/webs | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
-ffx() { fd -t=f -d=2 -e=html . /opt/.porn/text | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
 ffe() { fd -t=f -t=l -d=3 -a -H --size=-800k . | fzf --select-1 --query="$*" | sed 's/ /\\ /g' | xargs -r fileopen ;}
+
+ffa() { fd -H -L -d=3 -t=f -e=mp3 -e=m4a . ~/auds | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
+fff() { cat ~/.favedirs | sed "s%\~%$HOME%" | xargs fd -a -t=f -t=l . | fzf "$@" | xargs -r fileopen ;}
+ffv() { fd -H -L -d=3 -t=f -e=mp4 -e=webm -e=avi -e=mkv -e=rmvb . ~/vids | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
+ffw() { fd -d=1 -t=f . ~/docs/webs | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
+ffx() { fd -d=2 -t=f . /opt/.porn/text | fzf "$@" | sed 's/ /\\ /g' | xargs -r fileopen ;}
+
 ffo() {
     local files
     IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
