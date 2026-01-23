2b273287644b38939842f8792451f4074c4fe2aa
phantasea <phantasea@gmail.com>
2026-01-23 22:07:18 +0200

    yazi config

---
+++ yazi/init.lua
@@ -0,0 +1,28 @@
+require("smart-enter"):setup {
+    functionopen_multi = true,
+}
+
+require("full-border"):setup {
+    functiontype = ui.Border.ROUNDED,
+}
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("#6495ED"),
+		ui.Span(":"):fg("#87CEFA"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("#6495ED"),
+		ui.Span(" "),
+	})
+end, 500, Status.RIGHT)
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("#87CEFA")
+end, 500, Header.LEFT)
